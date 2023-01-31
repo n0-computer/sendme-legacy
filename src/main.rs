20241f9e5f4f0b0bdf076376934c886731b2fc39
dignifiedquire <me@dignifiedquire.com>
2023-01-31 13:12:34 +0100

    fix(bin): ensure progressbar works for stdout
    also redirects logs to STDERR

--- src/main.rs
+++ src/main.rs
@@ -112,7 +112,7 @@ const PROGRESS_STYLE: &str =
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> Result<()> {
     tracing_subscriber::registry()
-        .with(fmt::layer())
+        .with(fmt::layer().with_writer(std::io::stderr))
         .with(EnvFilter::from_default_env())
         .init();
 
@@ -266,6 +266,19 @@ async fn get_interactive(
         .await;
 
     let pb = ProgressBar::hidden();
+    pb.enable_steady_tick(std::time::Duration::from_millis(1));
+    pb.set_style(
+        ProgressStyle::with_template(PROGRESS_STYLE)
+            .unwrap()
+            .with_key(
+                "eta",
+                |state: &ProgressState, w: &mut dyn std::fmt::Write| {
+                    write!(w, "{:.1}s", state.eta().as_secs_f64()).unwrap()
+                },
+            )
+            .progress_chars("#>-"),
+    );
+
     let on_connected = || {
         let out_writer = &out_writer;
         async move {
@@ -293,18 +306,8 @@ async fn get_interactive(
                     "  {total_entries} file(s) with total transfer size {size}"
                 ))
                 .await;
-            pb.set_style(
-                ProgressStyle::with_template(PROGRESS_STYLE)
-                    .unwrap()
-                    .with_key(
-                        "eta",
-                        |state: &ProgressState, w: &mut dyn std::fmt::Write| {
-                            write!(w, "{:.1}s", state.eta().as_secs_f64()).unwrap()
-                        },
-                    )
-                    .progress_chars("#>-"),
-            );
             pb.set_length(size);
+            pb.reset();
             pb.set_draw_target(ProgressDrawTarget::stderr());
 
             Ok(())
@@ -317,12 +320,18 @@ async fn get_interactive(
             let name = name.map_or_else(|| hash.to_string(), |n| n);
             pb.set_message(format!("Receiving {name}..."));
 
+            // Wrap the reader to show progress.
+            // FIXME: this is only correct for the single file case atm.
+            let mut wrapped_reader = pb.wrap_async_read(&mut reader);
+
             if let Some(ref outpath) = out {
                 tokio::fs::create_dir_all(outpath)
                     .await
                     .context("Unable to create directory {outpath}")?;
                 let dirpath = std::path::PathBuf::from(outpath);
                 let filepath = dirpath.join(name);
+
+                // Create temp file
                 let (temp_file, dup) = tokio::task::spawn_blocking(|| {
                     let temp_file = tempfile::Builder::new()
                         .prefix("sendme-tmp-")
@@ -333,10 +342,10 @@ async fn get_interactive(
                 })
                 .await??;
                 let file = tokio::fs::File::from_std(dup);
-                let out = tokio::io::BufWriter::new(file);
-                // wrap for progress bar
-                let mut wrapped_out = pb.wrap_async_write(out);
-                tokio::io::copy(&mut reader, &mut wrapped_out).await?;
+                let mut file_buf = tokio::io::BufWriter::new(file);
+                tokio::io::copy(&mut wrapped_reader, &mut file_buf).await?;
+
+                // Rename temp file, to target name
                 let filepath2 = filepath.clone();
                 tokio::task::spawn_blocking(|| temp_file.persist(filepath2))
                     .await?
@@ -344,7 +353,7 @@ async fn get_interactive(
             } else {
                 // Write to OUT_WRITER
                 let mut stdout = tokio::io::stdout();
-                tokio::io::copy(&mut reader, &mut stdout).await?;
+                tokio::io::copy(&mut wrapped_reader, &mut stdout).await?;
             }
 
             Ok(reader)
